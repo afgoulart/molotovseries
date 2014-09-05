0479cb51d2cdfb010f266b1487737f8490768370
J. Gabriel Lima <jgabriel.ufpa@gmail.com>
2014-09-05 01:34:54 -0300

    melhorias visuais, inclusao do face

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    window.molotov = angular.module('Molotov', ['ngRoute', 'mongolabResourceHttp', 'ng-isotope']);
+    window.molotov = angular.module('Molotov', ['ngRoute', 'mongolabResourceHttp', 'ng-isotope', 'facebook']);
     window.molotov.config(function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
@@ -9,18 +9,18 @@
             .when('/serie/:id', {
                 templateUrl: 'app/views/serie.html'
             })
-            .when('/show/', {
+            .when('/show/:id', {
                 templateUrl: 'app/views/show.html'
             })
             .otherwise({
                 redirectTo: '/'
             });
-
         // $locationProvider.html5Mode(true);
         $locationProvider.hashPrefix('!');
-
     });
-
+    window.molotov.config(function(FacebookProvider) {
+        FacebookProvider.init('1501693240076943');
+    });
     window.molotov.constant('MONGOLAB_CONFIG', {
         API_KEY: 'F1Fk9-FjLLrA4c62rbTuCDmgkGg0sE4A',
         DB_NAME: 'molotovseries'
@@ -35,8 +35,8 @@
             };
         }
     ]);
-    window.molotov.controller('MolotovController', ['$scope', '$http', '$routeParams', 'Series',
-        function($scope, $http, $routeParams, Series) {
+    window.molotov.controller('MolotovController', ['$scope', '$http', '$routeParams', 'Series', 'Facebook',
+        function($scope, $http, $routeParams, Series, Facebook) {
             $scope.series = [];
             $scope.load = true;
             $scope.getAll = function() {
@@ -52,6 +52,7 @@
                     }
                 }).then(function(series) {
                     $scope.series = series;
+                    // console.log($scope.series)
                     $scope.load = false;
                 });
             };
@@ -59,9 +60,9 @@
                 $scope.load = true;
                 console.log($routeParams.id);
                 Series.getById($routeParams.id).then(function(serie) {
-                    console.log(serie);
+                    // console.log(serie);
                     $scope.serie = serie;
-                    window.localStorage[$routeParams.id] = JSON.stringify($scope.serie);
+                    // window.localStorage[$routeParams.id] = JSON.stringify($scope.serie);
                     $scope.load = false;
                 });
             };
@@ -70,348 +71,185 @@
 
                 var k = getParam('k');
                 var id = getParam('i');
-                Series.query({
-                    "_id.$oid": id
-                }).then(function(serie) {
-                    console.log(serie);
-                    // $scope.serie = series;
+                var p = getParam('p');
+                console.log(k);
+                Series.getById($routeParams.id).then(function(serie) {
+                    $scope.serie = serie;
                     $scope.load = false;
+                    var PLAYERS = {
+                        dp: "http://dropvideo.com/embed/{{id}}/",
+                        vdm: "http://vidto.me/embed-{{id}}-1280x720.html",
+                        amv: "http://allmyvideos.net/embed-{{id}}.html"
+                    };
+                    $scope.serie.currentlink = PLAYERS[p].replace("{{id}}", k);
                 });
-                // $scope.serie = JSON.parse(window.localStorage[id]);
-                // console.log($scope.serie);
-                // if (e !== null && p !== null && k !== null) {
-                //     var PLAYERS = {
-                //         dp: "http://dropvideo.com/embed/{{id}}/",
-                //         vdm: "http://vidto.me/embed-{{id}}.html",
-                //         amv: "http://allmyvideos.net/embed-{{id}}.html"
-                //     };
-
-                //     $scope.link = PLAYERS[p].replace("{{id}}", k);
-                //     $scope.episode = e;
-                // } else {}
-                // $scope.load = false;
             };
+
+            /*FACEBOOK*/
+            $scope.loginStatus = 'disconnected';
+            $scope.facebookIsReady = false;
+            $scope.user = null;
+
+            $scope.login = function() {
+                Facebook.login(function(response) {
+                    $scope.loginStatus = response.status;
+                });
+            };
+            $scope.logout = function() {
+                Facebook.logout(function(response) {
+                    $scope.loginStatus = response.status;
+                });
+            };
+
+            $scope.removeAuth = function() {
+                Facebook.api({
+                    method: 'Auth.revokeAuthorization'
+                }, function(response) {
+                    Facebook.getLoginStatus(function(response) {
+                        $scope.loginStatus = response.status;
+                    });
+                });
+            };
+
+            $scope.api = function() {
+                Facebook.api('/me', function(response) {
+                    $scope.user = response;
+                });
+            };
+
+            $scope.$watch(function() {
+                return Facebook.isReady();
+            }, function(newVal) {
+                if (newVal) {
+                    $scope.facebookIsReady = true;
+                }
+            });
         }
     ]);
 
     window.molotov.directive(
         "bnLazySrc",
         function($window, $document) {
-
-
-            // I manage all the images that are currently being
-            // monitored on the page for lazy loading.
             var lazyLoader = (function() {
-
-                // I maintain a list of images that lazy-loading
-                // and have yet to be rendered.
                 var images = [];
-
-                // I define the render timer for the lazy loading
-                // images to that the DOM-querying (for offsets)
-                // is chunked in groups.
                 var renderTimer = null;
                 var renderDelay = 100;
-
-                // I cache the window element as a jQuery reference.
                 var win = $($window);
-
-                // I cache the document document height so that 
-                // we can respond to changes in the height due to
-                // dynamic content.
                 var doc = $document;
                 var documentHeight = doc.height();
                 var documentTimer = null;
                 var documentDelay = 2000;
-
-                // I determine if the window dimension events 
-                // (ie. resize, scroll) are currenlty being 
-                // monitored for changes.
                 var isWatchingWindow = false;
 
-
-                // ---
-                // PUBLIC METHODS.
-                // ---
-
-
-                // I start monitoring the given image for visibility
-                // and then render it when necessary.
                 function addImage(image) {
-
                     images.push(image);
-
                     if (!renderTimer) {
-
                         startRenderTimer();
-
                     }
-
                     if (!isWatchingWindow) {
-
                         startWatchingWindow();
-
                     }
-
                 }
 
-
-                // I remove the given image from the render queue.
                 function removeImage(image) {
-
                     // Remove the given image from the render queue.
                     for (var i = 0; i < images.length; i++) {
-
                         if (images[i] === image) {
-
                             images.splice(i, 1);
                             break;
-
                         }
-
                     }
-
-                    // If removing the given image has cleared the
-                    // render queue, then we can stop monitoring 
-                    // the window and the image queue.
                     if (!images.length) {
-
                         clearRenderTimer();
-
                         stopWatchingWindow();
-
                     }
-
                 }
 
-
-                // ---
-                // PRIVATE METHODS.
-                // ---
-
-
-                // I check the document height to see if it's changed.
                 function checkDocumentHeight() {
-
-                    // If the render time is currently active, then 
-                    // don't bother getting the document height - 
-                    // it won't actually do anything.
                     if (renderTimer) {
-
                         return;
-
                     }
-
                     var currentDocumentHeight = doc.height();
-
-                    // If the height has not changed, then ignore - 
-                    // no more images could have come into view.
                     if (currentDocumentHeight === documentHeight) {
-
                         return;
-
                     }
-
-                    // Cache the new document height.
                     documentHeight = currentDocumentHeight;
-
                     startRenderTimer();
-
                 }
 
-
-                // I check the lazy-load images that have yet to 
-                // be rendered. 
                 function checkImages() {
-
-                    // Log here so we can see how often this 
-                    // gets called during page activity.
-                    console.log("Checking for visible images...");
-
                     var visible = [];
                     var hidden = [];
-
-                    // Determine the window dimensions.
                     var windowHeight = win.height();
                     var scrollTop = win.scrollTop();
-
-                    // Calculate the viewport offsets.
                     var topFoldOffset = scrollTop;
                     var bottomFoldOffset = (topFoldOffset + windowHeight);
-
-                    // Query the DOM for layout and seperate the
-                    // images into two different categories: those
-                    // that are now in the viewport and those that
-                    // still remain hidden.
                     for (var i = 0; i < images.length; i++) {
-
                         var image = images[i];
-
                         if (image.isVisible(topFoldOffset, bottomFoldOffset)) {
-
                             visible.push(image);
-
                         } else {
-
                             hidden.push(image);
-
                         }
-
                     }
-
-                    // Update the DOM with new image source values.
                     for (var i = 0; i < visible.length; i++) {
-
                         visible[i].render();
-
                     }
-
-                    // Keep the still-hidden images as the new 
-                    // image queue to be monitored.
                     images = hidden;
-
-                    // Clear the render timer so that it can be set
-                    // again in response to window changes.
                     clearRenderTimer();
-
-                    // If we've rendered all the images, then stop
-                    // monitoring the window for changes.
                     if (!images.length) {
-
                         stopWatchingWindow();
-
                     }
-
                 }
 
-
-                // I clear the render timer so that we can easily 
-                // check to see if the timer is running.
                 function clearRenderTimer() {
-
                     clearTimeout(renderTimer);
-
                     renderTimer = null;
-
                 }
 
-
-                // I start the render time, allowing more images to
-                // be added to the images queue before the render 
-                // action is executed.
                 function startRenderTimer() {
-
                     renderTimer = setTimeout(checkImages, renderDelay);
-
                 }
 
-
-                // I start watching the window for changes in dimension.
                 function startWatchingWindow() {
-
                     isWatchingWindow = true;
-
-                    // Listen for window changes.
                     win.on("resize.bnLazySrc", windowChanged);
                     win.on("scroll.bnLazySrc", windowChanged);
-
-                    // Set up a timer to watch for document-height changes.
                     documentTimer = setInterval(checkDocumentHeight, documentDelay);
-
                 }
 
-
-                // I stop watching the window for changes in dimension.
                 function stopWatchingWindow() {
-
                     isWatchingWindow = false;
-
-                    // Stop watching for window changes.
                     win.off("resize.bnLazySrc");
                     win.off("scroll.bnLazySrc");
-
-                    // Stop watching for document changes.
                     clearInterval(documentTimer);
-
                 }
 
-
-                // I start the render time if the window changes.
                 function windowChanged() {
-
                     if (!renderTimer) {
-
                         startRenderTimer();
-
                     }
-
                 }
-
-
-                // Return the public API.
                 return ({
                     addImage: addImage,
                     removeImage: removeImage
                 });
-
             })();
 
-
-            // ------------------------------------------ //
-            // ------------------------------------------ //
-
-
-            // I represent a single lazy-load image.
             function LazyImage(element) {
-
-                // I am the interpolated LAZY SRC attribute of 
-                // the image as reported by AngularJS.                  
                 var source = null;
-
-                // I determine if the image has already been 
-                // rendered (ie, that it has been exposed to the
-                // viewport and the source had been loaded).
                 var isRendered = false;
-
-                // I am the cached height of the element. We are 
-                // going to assume that the image doesn't change 
-                // height over time.
                 var height = null;
 
-
-                // ---
-                // PUBLIC METHODS.
-                // ---
-
-
-                // I determine if the element is above the given 
-                // fold of the page.
                 function isVisible(topFoldOffset, bottomFoldOffset) {
-
-                    // If the element is not visible because it 
-                    // is hidden, don't bother testing it.
                     if (!element.is(":visible")) {
-
                         return (false);
-
                     }
-
-                    // If the height has not yet been calculated, 
-                    // the cache it for the duration of the page.
                     if (height === null) {
-
                         height = element.height();
-
                     }
-
-                    // Update the dimensions of the element.
                     var top = element.offset().top;
                     var bottom = (top + height);
-
-                    // Return true if the element is:
-                    // 1. The top offset is in view.
-                    // 2. The bottom offset is in view.
-                    // 3. The element is overlapping the viewport.
                     return (
                         (
                             (top <= bottomFoldOffset) &&
@@ -426,75 +264,52 @@
                             (bottom >= bottomFoldOffset)
                         )
                     );
-
                 }
 
-
-                // I move the cached source into the live source.
                 function render() {
-
                     isRendered = true;
-
                     renderSource();
-
                 }
-                // I set the interpolated source value reported
-                // by the directive / AngularJS.
+
                 function setSource(newSource) {
                     source = newSource;
                     if (isRendered) {
                         renderSource();
                     }
                 }
-                // I load the lazy source value into the actual 
-                // source value of the image element.
+
                 function renderSource() {
-
                     element[0].src = source;
-
                 }
-                // Return the public API.
                 return ({
                     isVisible: isVisible,
                     render: render,
                     setSource: setSource
                 });
             }
-            // I bind the UI events to the scope.
+
             function link($scope, element, attributes) {
                 var lazyImage = new LazyImage(element);
-                // Start watching the image for changes in its
-                // visibility.
                 lazyLoader.addImage(lazyImage);
-                // Since the lazy-src will likely need some sort
-                // of string interpolation, we don't want to 
                 attributes.$observe(
                     "bnLazySrc",
                     function(newSource) {
-
                         lazyImage.setSource(newSource);
-
                     }
                 );
-                // When the scope is destroyed, we need to remove
-                // the image from the render queue.
                 $scope.$on(
                     "$destroy",
                     function() {
-
                         lazyLoader.removeImage(lazyImage);
-
                     }
                 );
             }
-            // Return the directive configuration.
             return ({
                 link: link,
                 restrict: "A"
             });
         }
     );
-
 })();
 
 function convertImgToBase64(url, callback, outputFormat) {
